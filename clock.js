06a985899a4f09c6b2c0b253126342c53cd0e8e2
David Miller <dmiller@mtechenterprises.com>
2019-03-19 12:35:39 -0500

    corrected timer code

--- clock.js
+++ clock.js
@@ -1,34 +1,33 @@
-let dayofweek  = document.getElementById('days');
-let hourofday = document.getElementById('hours');
-let minofhour = document.getElementById('minutes');
-let secofmin = document.getElementById('seconds');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
 
-// // //Set Launch Date
-let then = new Date('Nov 29, 2018 06:00:00').getTime();
+ //Set Launch Date
+const then = new Date('Nov 29, 2019 06:00:00').getTime();
 
-// // //Update By Seconds
-let intvl = setInterval(() => {
+ //Update By Seconds
+const intvl = setInterval(() => {
 
-//     //  //Get today's date and time (ms)
-    let now = new Date().getTime();
+  //Get today's date and time (ms)
+    const now = new Date().getTime();
 
-//     //  //Distance from then to now 
-    let distance = then - now;
+  //Distance from then to now 
+    const distance = then - now; 
 
-// //  //Calculations
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ //Calculations
+    let day = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minute = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let second = Math.floor((distance % (1000 * 60)) / 1000);
 
-//Display result
-    dayofweek.innerHTML = `${days}<span>days</span>`;
-    hourofday.innerHTML = `${hours}<span>hours</span>`;
-    minofhour.innerHTML = `${minutes}<span>minutes</span>`;
-    secofmin.innerHTML = `${seconds}<span>seconds</span>`;
+//Display result 
+    days.innerHTML = `${day}<span>days</span>`; 
+    hours.innerHTML =`${hour}<span>hours</span>`;
+    minutes.innerHTML =`${minute}<span>minutes</span>` ;
+    seconds.innerHTML =`${second}<span>seconds</span>` ;
 
 }, 1000)
-
 
 
  
